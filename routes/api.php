6559c8a34515bdd422a6c0ab976defe6f210a370
Ammar Abdelhalem <ammarabdelhalem19@gmail.com>
2026-02-08 20:43:19 +0200

    refactor(apiRoute): add ForceJsonResponse middleware and use it with all endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,19 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PointController;
+use App\Http\Middleware\ForceJsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // TODO Visioning the api with prefix v1
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/me', [AuthController::class,'me']);
-    Route::post('/logout', [AuthController::class,'logout']);
-    Route::post('/points', [PointController::class, 'store']);
+Route::middleware(ForceJsonResponse::class)->group(function (){
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/points', [PointController::class, 'learderboard']);
+    
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/me', [AuthController::class,'me']);
+        Route::post('/logout', [AuthController::class,'logout']);
+        Route::post('/points', [PointController::class, 'store']);
+    });
 });
