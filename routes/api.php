2e608104194025cae64aeb02eda831bcef683c90
Ammar Abdelhalem <ammarabdelhalem19@gmail.com>
2026-02-08 18:58:44 +0200

    refactor(apiRoutes): add new /points route and add todo comment

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PointController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+// TODO Visioning the api with prefix v1
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -10,4 +13,5 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class,'me']);
     Route::post('/logout', [AuthController::class,'logout']);
+    Route::post('/points', [PointController::class, 'store']);
 });
