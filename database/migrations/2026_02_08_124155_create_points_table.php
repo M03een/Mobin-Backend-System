8d2fdcfecc7cec1d98b933c7338f47d0f4bc0864
Ammar Abdelhalem <ammarabdelhalem19@gmail.com>
2026-02-08 15:11:40 +0200

    refactor(points): delete title field and replace it with amount

--- database/migrations/2026_02_08_124155_create_points_table.php
+++ database/migrations/2026_02_08_124155_create_points_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('points', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('title');
+            $table->integer('amount');
             $table->enum('type', [
                 'zikr',
                 'tasbeh'
