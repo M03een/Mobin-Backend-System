ef5982481e68c3d6251c4691f0d4e112ce3b2541
Ammar Abdelhalem <ammarabdelhalem19@gmail.com>
2026-02-09 14:42:23 +0200

    refactor(rankingAPI): add new routes for leaderboard

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\v1\AuthController;
+use App\Http\Controllers\v1\LeaderBoardController;
+use App\Http\Controllers\v1\PointController;
 use App\Http\Middleware\ForceJsonResponse;
-use App\Http\v1\Controllers\AuthController;
-use App\Http\v1\Controllers\PointController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,7 +11,10 @@ Route::prefix('v1')->group(function () {
     Route::middleware(ForceJsonResponse::class)->group(function (){
         Route::post('/register', [AuthController::class, 'register']);
         Route::post('/login', [AuthController::class, 'login']);
-        Route::get('/points', [PointController::class, 'learderboard']);
+        Route::get('/points/all', [LeaderBoardController::class, 'learderboard']);
+        Route::get('/points/month', [LeaderBoardController::class, 'month']);
+        Route::get('/points/week', [LeaderBoardController::class, 'week']);
+        Route::get('/points/day', [LeaderBoardController::class, 'day']);
         Route::post('password/forget', [AuthController::class, 'forgetPassword']);
         Route::post('password/reset', [AuthController::class, 'resetPassword']);
         
