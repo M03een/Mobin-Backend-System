ea66c2dc76d5097ca38e3a7601a6784f2e59913f
Ammar Abdelhalem <ammarabdelhalem19@gmail.com>
2026-02-09 13:08:53 +0200

    feat(passwordReset): add new password forget and password reset functions to AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ForgetPasswordRequest;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\ResetPasswordRequest;
+use App\Models\PasswordOtp;
 use App\Models\User;
-use Hash;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
@@ -52,6 +57,48 @@ class AuthController extends Controller
 
         return response()->json([
             'message' => 'Logged out successfully'
+        ]);
+    }
+
+    public function forgetPassword(ForgetPasswordRequest $request) {
+        $otp = random_int(100000, 999999);
+
+        PasswordOtp::updateOrCreate(
+            ['email' => $request->email],
+            [
+                'otp' => Hash::make($otp),
+                'expires_at' => Carbon::now()->addMinutes(10)
+            ]
+        );
+
+        Mail::raw("Your OTP is: $otp", function ($message) use ($request) {
+            $message->to($request->email)->subject("Password reset OTP");
+        });
+
+        return response()->json([
+            'message' => 'OTP sent'
+        ]);
+    }
+
+    public function resetPassword(ResetPasswordRequest $request) {
+        $record = PasswordOtp::where('email', $request->email)
+        ->where('expires_at', '>', now())
+        ->first();
+
+        if (!$record || !Hash::check($request->otp, $record->otp)) {
+            return response()->json(['message' => 'invalid OTP', 400]);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        $user->update([
+            'password' => Hash::make($request->password),
+        ]);
+
+        $record->delete();
+
+        return response()->json([
+            'message' => 'password reset successful'
         ]);
     }
 }
