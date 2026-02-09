f6b4c71f115505b6648e2594544ae4c01edb2e2c
Ammar Abdelhalem <ammarabdelhalem19@gmail.com>
2026-02-09 13:28:41 +0200

    feat(v1): add exitsting controllers v1 controllers

---
+++ app/Http/Controllers/v1/AuthController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\v1\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ForgetPasswordRequest;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\ResetPasswordRequest;
+use App\Models\PasswordOtp;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request) {
+        $user = User::create([
+            'username' => $request->username,
+            'email'=> $request->email,
+            'password'=> Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ], 201);
+    }
+
+    public function login(LoginRequest $request) {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                "message" => "The provided credentials are incorrect"
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            "user" => $user,
+            "token" => $token
+        ]);
+    }
+
+    public function me(Request $request) {
+        return response()->json($request->user());
+    }
+
+    public function logout(Request $request) {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ]);
+    }
+
+    public function forgetPassword(ForgetPasswordRequest $request) {
+        $otp = random_int(100000, 999999);
+
+        PasswordOtp::updateOrCreate(
+            ['email' => $request->email],
+            [
+                'otp' => Hash::make($otp),
+                'expires_at' => Carbon::now()->addMinutes(10)
+            ]
+        );
+
+        Mail::raw("Your OTP is: $otp", function ($message) use ($request) {
+            $message->to($request->email)->subject("Password reset OTP");
+        });
+
+        return response()->json([
+            'message' => 'OTP sent'
+        ]);
+    }
+
+    public function resetPassword(ResetPasswordRequest $request) {
+        $record = PasswordOtp::where('email', $request->email)
+        ->where('expires_at', '>', now())
+        ->first();
+
+        if (!$record || !Hash::check($request->otp, $record->otp)) {
+            return response()->json(['message' => 'invalid OTP', 400]);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        $user->update([
+            'password' => Hash::make($request->password),
+        ]);
+
+        $record->delete();
+
+        return response()->json([
+            'message' => 'password reset successful'
+        ]);
+    }
+}
---
+++ app/Http/Controllers/v1/PointController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\v1\Controllers;
+
+use App\Models\Point;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class PointController extends Controller
+{
+    public function learderboard()
+    {
+        $leaderboard = DB::table('users')->orderBy('points', 'desc')->limit(10)->get(['id','username', 'points']);
+        return response()->json($leaderboard);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        if ($request->type == "zikr") {
+            $user = User::find(Auth::id());
+            Point::create([
+                "user_id" => Auth::id(),
+                'type' => $request->type,
+                'amount' => $request->amount
+            ]);
+
+            $user->increment('points', $request->amount);
+
+            return response()->json([
+                'message' => $request->amount . " points add to user id " . Auth::id()
+            ], 201);
+        }
+
+        if ($request->type == "tasbeh") {
+            $user = User::find(Auth::id());
+            Point::create([
+                "user_id" => Auth::id(),
+                'type' => $request->type,
+                'amount' => $request->amount
+            ]);
+
+            $user->increment('points', $request->amount);
+
+            return response()->json([
+                'message' => $request->amount . " points add to user id " . Auth::id()
+            ], 201);
+        }
+
+        return response()->json([
+            "message" => "Unknown type was provided"
+        ], 400);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        // TODO Get the user's points
+    }
+}
