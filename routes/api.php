f50d634aeebbe1631c4e4b124c06c45a070f59c4
Ammar Abdelhalem <ammarabdelhalem19@gmail.com>
2026-02-09 13:31:06 +0200

    feat(v1): add new v1 prefix

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,23 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\PointController;
 use App\Http\Middleware\ForceJsonResponse;
+use App\Http\v1\Controllers\AuthController;
+use App\Http\v1\Controllers\PointController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// TODO Visioning the api with prefix v1
-Route::middleware(ForceJsonResponse::class)->group(function (){
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::get('/points', [PointController::class, 'learderboard']);
-    Route::post('password/forget', [AuthController::class, 'forgetPassword']);
-    Route::post('password/reset', [AuthController::class, 'resetPassword']);
-    
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::get('/me', [AuthController::class,'me']);
-        Route::post('/logout', [AuthController::class,'logout']);
-        Route::post('/points', [PointController::class, 'store']);
+Route::prefix('v1')->group(function () {
+    Route::middleware(ForceJsonResponse::class)->group(function (){
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::get('/points', [PointController::class, 'learderboard']);
+        Route::post('password/forget', [AuthController::class, 'forgetPassword']);
+        Route::post('password/reset', [AuthController::class, 'resetPassword']);
+        
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::get('/me', [AuthController::class,'me']);
+            Route::post('/logout', [AuthController::class,'logout']);
+            Route::post('/points', [PointController::class, 'store']);
+        });
     });
 });
