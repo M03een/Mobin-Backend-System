07c692fe2bab64f8aeffe6d2bf951f5087c939a3
Ammar Abdelhalem <ammarabdelhalem19@gmail.com>
2026-02-09 13:13:09 +0200

    feat(resetPassword): new Model and migration for password otp column

---
+++ app/Models/PasswordOtp.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class PasswordOtp extends Model
+{
+    use HasUuids;
+    protected $table = "password_otps";
+    protected $type = "string";
+    protected $increamenting = false;
+
+    protected $fillable = [
+        "email",
+        "otp",
+        "expires_at"
+    ];
+}
