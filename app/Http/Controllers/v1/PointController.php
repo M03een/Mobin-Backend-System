3b1e54f611ac6b74cc310e1b2a3a458debc943a8
Ammar Abdelhalem <ammarabdelhalem19@gmail.com>
2026-02-18 17:48:01 +0200

    refactor(points): update the PointControllers to calculate points in backend

--- app/Http/Controllers/v1/PointController.php
+++ app/Http/Controllers/v1/PointController.php
@@ -16,70 +16,190 @@ class PointController extends Controller
     {
         $request->validate([
             'type' => 'required|in:zikr,tasbeh',
-            'amount' => 'required|integer|min:1'
         ]);
 
         $userId = Auth::id();
-        $amount = $request->amount;
         $type = $request->type;
 
-        DB::transaction(function () use ($userId, $amount, $type) {
+        if ($request->type == "zikr") {
+            DB::transaction(function () use ($userId, $type) {
 
-            Point::create([
-                "user_id" => $userId,
-                'type' => $type,
-                'amount' => $amount,
-                'created_at' => now(),
-            ]);
-
-            DB::table('user_points_total')->updateOrInsert(
-                ['user_id' => $userId],
-                [
-                    'points' => DB::raw("COALESCE(points, 0) + $amount"),
+                Point::create([
+                    "user_id" => $userId,
+                    'type' => $type,
+                    'amount' => 25,
                     'created_at' => now(),
-                    'updated_at' => now(),
-                ]
-            );
-            
-
-            DB::table('user_points_daily')->updateOrInsert(
-                ['user_id' => $userId, 'date' => now()->toDateString()],
-                ['points' => DB::raw("points + $amount")]
-            );
-
-            DB::table('user_points_monthly')->updateOrInsert(
-                ['user_id' => $userId, 'month' => now()->format('Y-m')],
-                ['points' => DB::raw("points + $amount")]
-            );
-
-            $weekKey = now()->year . '-' . now()->weekOfYear;
-
-            $weekly = DB::table('user_points_weekly')
-                ->where('user_id', $userId)
-                ->where('week', $weekKey);
-
-            if ($weekly->exists()) {
-                $weekly->increment('points', $amount);
-            } else {
-                DB::table('user_points_weekly')->insert([
-                    'user_id' => $userId,
-                    'week' => $weekKey,
-                    'points' => $amount,
                 ]);
-            }
-
-            $user = User::find($userId);
-            $user->increment('points', $amount);
-
-            if ($type == "zikr") {
+    
+                DB::table('user_points_total')->upsert(
+                    [
+                        [
+                            'user_id' => $userId,
+                            'created_at' => now(),
+                            'updated_at' => now(),
+                            'points' => 25,
+                        ]
+                    ],
+                    ['user_id'],
+                    [
+                        'points' => DB::raw('points + 25'),
+                        'updated_at' => now(),
+                    ]
+                );   
+                
+                
+    
+                DB::table('user_points_daily')->upsert(
+                    [
+                        [
+                            'user_id' => $userId,
+                            'date' => now()->toDateString(),
+                            'points' => 25,
+                        ]
+                    ],
+                    ['user_id', 'date'],
+                    [
+                        'points' => DB::raw('points + 25'),
+                        'date' => now()->toDateString(),
+                    ]
+                );                                     
+                
+    
+                DB::table('user_points_monthly')->upsert(
+                    [
+                        [
+                            'user_id' => $userId,
+                            'month' => now()->format('Y-m'),
+                            'points' => 25
+                        ]
+                    ],
+                    ['user_id'],
+                    [
+                        'month' => now()->format('Y-m'),
+                        'points' => DB::raw('points + 25')
+                    ]
+                    );
+                
+    
+                $weekKey = now()->year . '-' . now()->weekOfYear;
+    
+                $weekly = DB::table('user_points_weekly')
+                    ->where('user_id', $userId)
+                    ->where('week', $weekKey);
+    
+                if ($weekly->exists()) {
+                    $weekly->increment('points', 25);
+                } else {
+                    DB::table('user_points_weekly')->insert([
+                        'user_id' => $userId,
+                        'week' => $weekKey,
+                        'points' => 25,
+                    ]);
+                }
+    
+                $user = User::find($userId);
+                $user->increment('points', 25);
                 $user->last_point_at = now();
                 $user->save();
-            }
-        });
+            });
 
-        return response()->json([
-            'message' => "$amount points added to user id $userId"
-        ], 201);
+            $user = auth()->user();
+            $user->updateStreak();
+            return response()->json([
+                'message' => "25 points added to user id $userId"
+            ], 201);
+        } elseif ($request->type == "tasbeh") {
+            DB::transaction(function () use ($userId, $type) {
+
+                Point::create([
+                    "user_id" => $userId,
+                    'type' => $type,
+                    'amount' => 25,
+                    'created_at' => now(),
+                ]);
+    
+                DB::table('user_points_total')->upsert(
+                    [
+                        [
+                            'user_id' => $userId,
+                            'created_at' => now(),
+                            'updated_at' => now(),
+                            'points' => 25,
+                        ]
+                    ],
+                    ['user_id'],
+                    [
+                        'points' => DB::raw('points + 25'),
+                        'updated_at' => now(),
+                    ]
+                );   
+                
+                
+    
+                DB::table('user_points_daily')->upsert(
+                    [
+                        [
+                            'user_id' => $userId,
+                            'date' => now()->toDateString(),
+                            'points' => 5,
+                        ]
+                    ],
+                    ['user_id', 'date'],
+                    [
+                        'points' => DB::raw('points + 5'),
+                        'date' => now()->toDateString(),
+                    ]
+                );                                     
+                
+    
+                DB::table('user_points_monthly')->upsert(
+                    [
+                        [
+                            'user_id' => $userId,
+                            'month' => now()->format('Y-m'),
+                            'points' => 5
+                        ]
+                    ],
+                    ['user_id'],
+                    [
+                        'month' => now()->format('Y-m'),
+                        'points' => DB::raw('points + 5')
+                    ]
+                    );
+                
+    
+                $weekKey = now()->year . '-' . now()->weekOfYear;
+    
+                $weekly = DB::table('user_points_weekly')
+                    ->where('user_id', $userId)
+                    ->where('week', $weekKey);
+    
+                if ($weekly->exists()) {
+                    $weekly->increment('points', 5);
+                } else {
+                    DB::table('user_points_weekly')->insert([
+                        'user_id' => $userId,
+                        'week' => $weekKey,
+                        'points' => 5,
+                    ]);
+                }
+    
+                $user = User::find($userId);
+                $user->increment('points', 5);
+                $user->last_point_at = now();
+                $user->save();
+            });
+
+            $user = auth()->user();
+            $user->updateStreak();
+            return response()->json([
+                'message' => "5 points added to user id $userId"
+            ], 201);
+        } else {
+            return response()->json([
+                "message" => "wrong type"
+            ], 400);
+        }
     }
 
     /**
