7dd77a1be8e75740d61ef999aadec059fd217340
Ammar Abdelhalem <ammarabdelhalem19@gmail.com>
2026-02-18 18:34:30 +0200

    fix(users): fix points relationship by add return

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable
     }
 
     public function points() {
-        $this->hasMany(Point::class);
+        return $this->hasMany(Point::class);
     }
 
     public function updateStreak()
